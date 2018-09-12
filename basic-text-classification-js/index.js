2cebca85a0400cf0da104fdfbbbf1fe1f36aeca2
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-09-12 19:59:16 +0200

    Setup for client side

--- basic-text-classification-js/index.js
+++ basic-text-classification-js/index.js
@@ -2,4 +2,8 @@
 
 import * as tf from '@tensorflow/tfjs';
 
-const model = await tf.loadModel('https://foo.bar/tfjs_artifacts/model.json');
+const run = async () => {
+  const model = await tf.loadModel('https://foo.bar/tfjs_artifacts/model.json');
+};
+
+run();
