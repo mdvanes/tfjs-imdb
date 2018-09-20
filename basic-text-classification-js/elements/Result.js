b155256a902219e0fb59fc4219fdf9b0b8fae9c9
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-09-20 09:43:31 +0200

    Add LitElement

--- basic-text-classification-js/elements/Result.js
+++ basic-text-classification-js/elements/Result.js
@@ -1,28 +1,41 @@
-import {html, render} from 'lit-html';
+import {LitElement, html, property} from '@polymer/lit-element';
 
-// let myTemplate = (data) => html`
-//   <h1>${data.title}</h1>
-//   <p>${data.body}</p>`;
-//
-// const result = myTemplate({title: 'Hello', body: 'lit-html is cool'});
-// render(result, document.body);
+class Result extends LitElement {
 
-const data = {title: 'Hello', body: 'lit-html is cool'};
+  static get properties() {
+    return {
+      mood: {type: String}
+    };
+  }
 
-// See https://github.com/mdvanes/realtime-planner/blob/a611aaba2febfa0723092b09d139cc78a0ba3bd4/src/LatestTweet.ts
-class Result extends HTMLElement {
   constructor() {
     super();
-    // TODO why is polyfill in index.html needed? <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.0.3/custom-elements-es5-adapter.js"></script>
-    this.render();
+    this.mood = 'happy';
   }
 
   render() {
-    const header = html`
-      <h1>${data.title}</h1>
-      <p>${data.body}</p>`;
-    render(header, this);
+    return html`<style> .mood { color: green; } </style>
+          Web Components are <span class="mood">${this.mood}</span>!`;
   }
+
 }
 
 customElements.define('t-result', Result);
+
+
+// import { LitElement, html } from 'lit-html-element';
+//
+// const data = {title: 'Hello', body: 'lit-html is cool'};
+//
+// // TODO why is polyfill in index.html needed? <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.0.3/custom-elements-es5-adapter.js"></script>
+// // See https://github.com/mdvanes/realtime-planner/blob/a611aaba2febfa0723092b09d139cc78a0ba3bd4/src/LatestTweet.ts
+//
+// class Result extends LitElement {
+//   render() {
+//     return html`
+//       <h1>${data.title}</h1>
+//       <p>${data.body}</p>`;
+//   }
+// }
+//
+// customElements.define('t-result', Result);
