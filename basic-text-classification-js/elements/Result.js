94036a00e206e8d04ddc8450ae6b1be96a7f3d3a
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-09-20 09:50:21 +0200

    Add LitElement

--- basic-text-classification-js/elements/Result.js
+++ basic-text-classification-js/elements/Result.js
@@ -1,41 +1,30 @@
 import {LitElement, html, property} from '@polymer/lit-element';
 
+const data = {title: 'Hello', body: 'General Kenobi'};
+
+// See https://github.com/mdvanes/realtime-planner/blob/a611aaba2febfa0723092b09d139cc78a0ba3bd4/src/LatestTweet.ts
 class Result extends LitElement {
 
   static get properties() {
     return {
-      mood: {type: String}
+      title: {type: String},
+      body: {type: String}
     };
   }
 
   constructor() {
     super();
-    this.mood = 'happy';
+    this.title = data.title;
+    this.body = data.body;
   }
 
   render() {
-    return html`<style> .mood { color: green; } </style>
-          Web Components are <span class="mood">${this.mood}</span>!`;
+    return html`
+        <style> .mood { color: green; } </style>
+            <h1><span class="mood">${this.title}</span> there</h1>
+            <p>${this.body}</p>`;
   }
 
 }
 
 customElements.define('t-result', Result);
-
-
-// import { LitElement, html } from 'lit-html-element';
-//
-// const data = {title: 'Hello', body: 'lit-html is cool'};
-//
-// // TODO why is polyfill in index.html needed? <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.0.3/custom-elements-es5-adapter.js"></script>
-// // See https://github.com/mdvanes/realtime-planner/blob/a611aaba2febfa0723092b09d139cc78a0ba3bd4/src/LatestTweet.ts
-//
-// class Result extends LitElement {
-//   render() {
-//     return html`
-//       <h1>${data.title}</h1>
-//       <p>${data.body}</p>`;
-//   }
-// }
-//
-// customElements.define('t-result', Result);
